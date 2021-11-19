24c37d0faa5cf2e39ea0ed05e1bb9df6f5d42e9c
Delxys <bausdelxys@gmail.com>
2021-11-19 13:25:40 +0300

    unit tests

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
+###################################################################################
+# Build
+###################################################################################
+
 # Base dotnet image for building
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
 
@@ -11,18 +15,36 @@ COPY ./ .
 RUN dotnet restore testapp2.sln
 
 # Build
-RUN dotnet build testapp2.sln -c Release -o ./publish
+RUN dotnet publish testapp2.sln -c Release -o ./publish --no-restore
+
+###################################################################################
+# Prepare and execution
+###################################################################################
 
 FROM mcr.microsoft.com/dotnet/aspnet:5.0-alpine
 
 # Set app directory
 WORKDIR /app
 
+# Add user/group with write permissions to work directory
+# RUN adduser -S -D -H -u 1001 -s /sbin/nologin -G root -g user user
+RUN addgroup -g 1000 -S user && \
+    adduser -u 1000 -S user -G user -h "/app"
+
 # Copy files with default permissions
 COPY --from=build /src/publish .
+ 
+# Grant read/write permissions
+# RUN chgrp -R 0 /app 
+# RUN chmod -R g+rX /app
+RUN chmod -s /app
+
+# Set work user
+USER user
 
 # Expose app port
 EXPOSE 5000
 ENV ASPNETCORE_URLS=http://*:5000
+ENV ASPNETCORE_ENVIRONMENT=Development 
 
 ENTRYPOINT ["dotnet", "testapp2.dll"]
--- src/testapp2.Tests/ControllerTests/IntegralControllerTests.cs
+++ src/testapp2.Tests/ControllerTests/IntegralControllerTests.cs
@@ -14,11 +14,47 @@ namespace testapp2.Tests.ControllerTests
         [Fact]
         public void GetIntegral_Success()
         {
-            IntegralInput integralInput = new IntegralInput() { A = 1, B = 9, N = 1000, Integral = "(x*x)/2" };
+            IntegralInput integralInput = new IntegralInput() { A = 1, B = 9, N = 1000, Integral = "x*x/2" };
             IntegralController integralController = new IntegralController();
             var testRes = integralController.Post(integralInput);
             var expectedRes = 121.333;
-            Assert.Equal(testRes.Answer, expectedRes, 3);
+            Assert.Equal(testRes.Answer, expectedRes, 1);
+        }
+        [Fact]
+        public void GetBigIntegral_Success()
+        {
+            IntegralInput integralInput = new IntegralInput() { A = 1, B = 1200, N = 1000, Integral = "x*x*x/(x+3)" };
+            IntegralController integralController = new IntegralController();
+            var testRes = integralController.Post(integralInput);
+            var expectedRes = 573850638.0971241;
+            Assert.Equal(testRes.Answer, expectedRes, 1);
+        }
+        [Fact]
+        public void GetIntegralBlowerA()
+        {
+            IntegralInput integralInput = new IntegralInput() { A = 9, B = 1, N = 1000, Integral = "x*x" };
+            IntegralController integralController = new IntegralController();
+            var testRes = integralController.Post(integralInput);
+            var expectedRes = -242.666;
+            Assert.Equal(testRes.Answer, expectedRes, 1);
+        }
+        [Fact]
+        public void GetNullException()
+        {
+            IntegralInput integralInput = new IntegralInput() { A = 0, B = 0, N = 0, Integral = "string" };
+            IntegralController integralController = new IntegralController();
+            Action testRes = ()=> integralController.Post(integralInput);
+            Assert.Throws<NullReferenceException>(testRes);
+        }
+
+       
+        [Fact]
+        public void GetIntegralNIsOdd()
+        {
+            IntegralInput integralInput = new IntegralInput() { A = 1, B = 2, N = 33, Integral = "x*x" };
+            IntegralController integralController = new IntegralController();
+            Action testRes = () => integralController.Post(integralInput);
+            Assert.Throws<ArgumentException>(testRes);
         }
     }
 }
